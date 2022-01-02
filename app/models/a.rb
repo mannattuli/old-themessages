5d974654c4f8acdb70c28d1a124360f34aa67c12
Mannat <mannattuli13@gmail.com>
2022-01-02 23:30:07 +0530

    D

--- app/models/a.rb
+++ app/models/a.rb
@@ -3,7 +3,7 @@ class A < ApplicationRecord
     belongs_to :board
     has_many :comments
     validates :body, :subject, presence: true
-    validates :body, length: { maximum: 500 }
-    validates :subject, length: { maximum: 100 }
+    validates :body, length: { minimum: 20, maximum: 500 }
+    validates :subject, length: { minimum: 6, maximum: 80 }
     validates :image, length: { maximum: 30 }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
     belongs_to :a
     belongs_to :user
     validates :comment, presence: true
-    validates :comment, length: { maximum: 500 }
+    validates :comment, length: { minimum: 7, maximum: 500 }
 end
